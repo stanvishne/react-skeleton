b4d416d0284a63c5dadb597ab1fc4a0d81d397ab
Stan Vishne <stanvishne@gmail.com>
2016-01-28 17:53:10 +0200

    skeleton

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var List = require('./components/List.jsx');
+
+ReactDom.render(<List />, document.getElementById('ingredients'));
+
